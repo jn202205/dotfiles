557f7ec882b025173a2d9b1d229686d6e571de8d
Jonathan Newman <jn202205@gmail.com>
2015-08-30 21:40:12 -0700

    setup script to symlink files in home directory to dotfiles directory

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+############################
+# setup.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+# list of files/folders to symlink in homedir
+files="gemrc git_template gitconfig gitignore_global ideavimrc railsrc rspec tmux.conf vimrc vimrc.bundles vim zshrc"
+
+##########
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+echo "copying bin/battery to ~"
+
+# symlink does work for battery script
+mkdir ~/bin
+cp bin/battery ~/bin/battery
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file $olddir
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
