a8731c4ab6bdd0eb471f2bc5030b6eba218b52af
Jonathan Newman <jn202205@gmail.com>
2015-08-30 22:46:35 -0700

    add README and brew.sh

---
+++ brew.sh
@@ -0,0 +1,14 @@
+cd ~
+
+echo "  > Updating homebrew..."
+brew update
+
+echo "  > Installing packages with homebrew.."
+
+brew install macvim --override-system-vim
+brew install reattach-to-user-namespace tmux cmake ctags node tig heroku the_silver_searcher postgresql brew-cask
+brew tap caskroom/versions
+brew tap caskroom/cask
+brew cask install java7
+
+echo "***** DONE *****"
