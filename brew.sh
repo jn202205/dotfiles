ec664e6d5703b324ef0f8b833d17d9edfbf9acb4
Jonathan Newman <jn202205@gmail.com>
2015-08-31 21:41:00 -0700

    add rbenv and ruby-build to brew.sh

--- brew.sh
+++ brew.sh
@@ -6,7 +6,7 @@ brew update
 echo "  > Installing packages with homebrew.."
 
 brew install macvim --override-system-vim
-brew install reattach-to-user-namespace tmux cmake ctags node tig heroku the_silver_searcher postgresql brew-cask
+brew install reattach-to-user-namespace tmux cmake ctags node tig heroku the_silver_searcher postgresql brew-cask rbenv ruby-build
 brew tap caskroom/versions
 brew tap caskroom/cask
 brew cask install java7
