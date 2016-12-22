f019f424602b6b92222334f844c52a769c095261
Jonathan Newman <j24newman@gmail.com>
2016-12-22 14:59:30 -0800

    Add rbenv-gemset

--- brew.sh
+++ brew.sh
@@ -6,7 +6,7 @@ brew update
 echo "  > Installing packages with homebrew.."
 
 brew install macvim --override-system-vim
-brew install reattach-to-user-namespace tmux cmake ctags node the_silver_searcher postgresql brew-cask rbenv ruby-build
+brew install reattach-to-user-namespace tmux cmake ctags node the_silver_searcher brew-cask rbenv ruby-build rbenv-gemset
 brew tap caskroom/versions
 brew tap caskroom/cask
 brew cask install java7
